b48d4c8c8b0c902e34bea2c578a57c5d94427587
Aja Blanco <59661417+ajablanco@users.noreply.github.com>
2020-03-13 10:51:20 -0700

    MVP and Stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import styled from "styled-components";
+import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = styled.h1`
+  text-align: center;
+  width: 100%;
+`;
+
+const PlayerCard = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  width: 300px;
+  border: 1px solid black;
+  margin: 2% auto;
+  text-align: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 20px;
+`;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players").then(response => {
+      console.log(response.data);
+      this.setState({
+        players: response.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <NavBar />
+        <Header>
+          Women's World Cup Players
+        </Header>
+        {this.state.players.map(players => {
+          return (
+            <PlayerCard>
+              <div>
+                <p>Name: {players.name}</p>
+                <p>Country: {players.country}</p>
+                <p>Searches: {players.searches}</p>
+              </div>
+            </PlayerCard>
+          );
+        })}
+      </Container>
+    );
+  }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from "@testing-library/react";
+import '@testing-library/jest-dom';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+  render(<App />);
 });
+
+test('header shows up', () => {
+  //arrange
+  const { getByText } = render(< App />);
+
+  //act
+  const headerTitle = getByText(/women's world cup players/i);
+
+  //assert that it is being rendered
+  expect(headerTitle).toBeInTheDocument()
+});
+
